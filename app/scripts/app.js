59658c871654b1af1c391d1ae05a30122d74d81a
Jörg Weber <weber.joerg.h@gmail.com>
2014-10-25 14:00:30 +0200

    * move cards service in separate file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,43 +24,4 @@ angular
   .config(function (localStorageServiceProvider) {
     localStorageServiceProvider
       .setPrefix('mtg');
-  })
-  .service('cards', ['$q', '$http', 'localStorageService', function ($q, $http) {
-    var allCards = [],
-      setList = [],
-      cardsDb = TAFFY();
-
-    window.cardsDb = cardsDb;
-    return {
-      filter: function(){
-        return cardsDb(arguments).get();
-      },
-      fetchCards: function () {
-        var defer = $q.defer();
-        $http.get('/data/ALL_SETS.json')
-          .success(function (response) {
-
-            _.each(response, function(mtgSet){
-              var cards = _.map(mtgSet.cards, function(card){
-                card.setCode = mtgSet.code;
-                card.setName = mtgSet.name;
-                return card;
-              });
-              allCards = allCards.concat(cards);
-            });
-            cardsDb.insert(allCards);
-            defer.resolve(cardsDb().get());
-          });
-        return defer.promise;
-      },
-      fetchSetList: function () {
-        var defer = $q.defer();
-        $http.get('/data/SET_LIST.json')
-          .success(function (response) {
-            setList = response;
-            defer.resolve(setList);
-          });
-        return defer.promise;
-      }
-    };
   });
---
+++ app/scripts/services/cards-service.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular.module('mtgApp')
+  .service('cards', ['$q', '$http', 'localStorageService', function ($q, $http) {
+    var allCards = [],
+      setList = [],
+      cardsDb = TAFFY();
+
+    window.cardsDb = cardsDb;
+
+    function filter() {
+      return cardsDb(arguments).get();
+    }
+
+    function fetchCards() {
+      var defer = $q.defer();
+      $http.get('/data/ALL_SETS.json')
+        .success(function (response) {
+
+          _.each(response, function (mtgSet) {
+            var cards = _.map(mtgSet.cards, function (card) {
+              card.setCode = mtgSet.code;
+              card.setName = mtgSet.name;
+              return card;
+            });
+            allCards = allCards.concat(cards);
+          });
+          cardsDb.insert(allCards);
+          defer.resolve(cardsDb().get());
+        });
+      return defer.promise;
+    }
+
+    function fetchSetList() {
+      var defer = $q.defer();
+      $http.get('/data/SET_LIST.json')
+        .success(function (response) {
+          setList = response;
+          defer.resolve(setList);
+        });
+      return defer.promise;
+    }
+
+    return {
+      filter: filter,
+      fetchCards: fetchCards,
+      fetchSetList: fetchSetList
+    };
+  }]);

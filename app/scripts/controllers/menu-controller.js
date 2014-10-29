163fefbf10475ba6025b6c34de72a4cc9520e5d4
Jörg Weber <weber.joerg.h@gmail.com>
2014-10-29 15:38:02 +0100

    * watch total deck count to update deck menu

--- app/scripts/controllers/menu-controller.js
+++ app/scripts/controllers/menu-controller.js
@@ -3,4 +3,7 @@
 angular.module('mtgApp')
 .controller('MenuController', ['$scope', 'decks', function($scope, decks){
     $scope.allDecks = decks.getAll();
+    $scope.$watch(decks.count, function(){
+      $scope.allDecks = decks.getAll();
+    });
   }]);

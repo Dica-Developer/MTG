6904b824dea30ed2770759b270404b1cf5765f4e
Jörg Weber <weber.joerg.h@gmail.com>
2014-10-29 10:33:19 +0100

    * separated card modal controller

--- app/scripts/controllers/card-explorer-controller.js
+++ app/scripts/controllers/card-explorer-controller.js
@@ -31,17 +31,6 @@ angular.module('mtgApp')
       return out;
     };
   })
-  .controller('CardModalController', ['$scope', '$modalInstance', 'card', function ($scope, $modalInstance, card) {
-    $scope.card = card;
-
-    $scope.ok = function () {
-      $modalInstance.close();
-    };
-
-    $scope.cancel = function () {
-      $modalInstance.dismiss('cancel');
-    };
-  }])
   .controller('CardExplorerController', [
     '$scope',
     '$modal',
---
+++ app/scripts/controllers/card-modal-controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('mtgApp')
+  .controller('CardModalController', ['$scope', '$modalInstance', 'card', 'showCounter', function ($scope, $modalInstance, card, showCounter) {
+    $scope.card = card;
+    $scope.showCounter = showCounter;
+
+    $scope.ok = function () {
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  }])

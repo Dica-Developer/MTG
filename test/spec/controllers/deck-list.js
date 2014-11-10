bb5f3999c382d1c5b24865e36dcf1536ad515458
Jörg Weber <weber.joerg.h@gmail.com>
2014-11-10 22:03:09 +0100

    * add deck list controller spec

---
+++ test/spec/controllers/deck-list.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('Controller: DeckListController', function () {
+
+  // load the controller's module
+  beforeEach(module('mtgApp'));
+
+  var $rootScope,
+    $state,
+    scope,
+    decks;
+
+  beforeEach(inject(function ($injector) {
+    $rootScope = $injector.get('$rootScope');
+    decks = $injector.get('decks');
+    $state = $injector.get('$state');
+    var $controller = $injector.get('$controller');
+    scope = $rootScope.$new();
+    $controller('DeckListController', {
+      $scope: scope,
+      decks: decks,
+      $state: $state
+    });
+  }));
+
+  it('$scope.addNew should call decks.addNew() and $state.go', function () {
+    spyOn(decks, 'addNew').andCallThrough();
+    spyOn($state, 'go');
+    scope.addDeck();
+    expect(decks.addNew).toHaveBeenCalled();
+    expect($state.go).toHaveBeenCalled();
+    expect($state.go.mostRecentCall.args[0]).toBe('deck-builder');
+  });
+});

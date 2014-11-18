f9af8b2d8fc9769088f56bb907c1254692dbf030
Jörg Weber <weber.joerg.h@gmail.com>
2014-11-18 21:33:54 +0100

    * add mana cost directive specs

---
+++ test/spec/directives/mana-cost.js
@@ -0,0 +1,50 @@
+'use strict';
+
+describe('Directive: manaCost', function () {
+  var $compile,
+    $rootScope;
+
+  beforeEach(module('mtgApp'));
+  beforeEach(module('templates'));
+
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it('Should add mana-cost for numbers 0-9', function () {
+    var numbers = _.range(10);
+    var element = $compile('<mana-cost x-mana-cost="{{manaCost}}" x-size="16"></mana-cost>')($rootScope);
+    numbers.forEach(function (number) {
+      $rootScope.manaCost = number;
+      $rootScope.$digest();
+      expect(element.find('img').attr('ng-src')).toBe('http://mtgimage.com/symbol/mana/' + number + '.svg');
+    });
+  });
+
+  it('Should add correct symbol for mana', function () {
+    var manaSymbols = ['W', 'U', 'B', 'R', 'G', 'X', 'P', 'S','T', 'Q', 'C'];
+    manaSymbols.forEach(function (symbol) {
+      $rootScope.symbol = symbol;
+      var element = $compile('<mana-cost x-mana-cost="{{symbol}}" x-size="16"></mana-cost>')($rootScope);
+      $rootScope.$digest();
+      expect(element.find('img').attr('ng-src')).toBe('http://mtgimage.com/symbol/mana/' + symbol.toLowerCase() + '.svg');
+    });
+  });
+
+  it('Should leave ng-src blank if mana symbol doesn\'t match', function () {
+    $rootScope.symbol = 'd';
+    var element = $compile('<mana-cost x-mana-cost="{{symbol}}" x-size="16"></mana-cost>')($rootScope);
+    $rootScope.$digest();
+    expect(element.find('img').attr('ng-src')).toBeUndefined();
+  });
+
+  it('Should add mana if mana cost is devided by a slash ({B/R})', function () {
+    var symbol = '{B/R}';
+      $rootScope.symbol = symbol;
+    var element = $compile('<mana-cost x-mana-cost="{{symbol}}" x-size="16"></mana-cost>')($rootScope);
+    $rootScope.$digest();
+    expect(element.find('img').attr('ng-src')).toBe('http://mtgimage.com/symbol/mana/br.svg');
+  });
+});
+

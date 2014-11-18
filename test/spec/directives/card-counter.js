4426310191acbb0c1ec18802174b1aadfd246fe5
Jörg Weber <weber.joerg.h@gmail.com>
2014-11-18 23:24:16 +0100

    * add card counter specs

---
+++ test/spec/directives/card-counter.js
@@ -0,0 +1,38 @@
+'use strict';
+
+describe('Directive: cardCounter', function () {
+  var $compile,
+    $rootScope,
+    cards,
+    ownCards;
+
+  beforeEach(module('mtgApp'));
+  beforeEach(module('templates'));
+
+  beforeEach(inject(function (_$compile_, _$rootScope_, _ownCards_, _cards_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+    cards = _cards_;
+    ownCards = _ownCards_;
+
+    cards.db.insert(cardsExcerpt);
+  }));
+
+  it('Should display correct amount of own cards', function () {
+    $rootScope.multiverseid = 279;
+    var element = $compile('<card-counter x-card-id="{{multiverseid}}" ></card-counter>')($rootScope);
+    $rootScope.$digest();
+    expect(element.find('.label-default').text()).toBe('0');
+
+    ownCards.addCard('279');
+
+    $rootScope.$digest();
+    expect(element.find('.label-default').text()).toBe('1');
+
+    ownCards.removeCard('279');
+
+    $rootScope.$digest();
+    expect(element.find('.label-default').text()).toBe('0');
+  });
+});
+

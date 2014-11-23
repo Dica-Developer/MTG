51605990d0a91c02d4da154ccdcd7a31833db40a
Jörg Weber <weber.joerg.h@gmail.com>
2014-11-23 21:22:08 +0100

    * extend deck list controller specs

--- test/spec/controllers/deck-list.js
+++ test/spec/controllers/deck-list.js
@@ -8,7 +8,8 @@ describe('Controller: DeckListController', function () {
   var $rootScope,
     $state,
     scope,
-    decks;
+    decks,
+    event;
 
   beforeEach(inject(function ($injector) {
     $rootScope = $injector.get('$rootScope');
@@ -23,6 +24,7 @@ describe('Controller: DeckListController', function () {
       decks: decks,
       $state: $state
     });
+    event = jasmine.createSpyObj('event', ['preventDefault', 'stopImmediatePropagation']);
   }));
 
   it('$scope.addNew should call decks.addNew() and $state.go', function () {
@@ -32,5 +34,22 @@ describe('Controller: DeckListController', function () {
     expect(decks.addNew).toHaveBeenCalled();
     expect($state.go).toHaveBeenCalled();
     expect($state.go.calls.mostRecent().args[0]).toBe('deck-builder');
+  });
+
+  it('$scope.removeDeck should call decks.removeDeck() with correct id', function () {
+    spyOn(decks, 'removeDeck');
+    var deck = decks.addNew();
+    scope.removeDeck(event, deck.options.id);
+    expect(decks.removeDeck).toHaveBeenCalledWith(deck.options.id);
+    expect(event.stopImmediatePropagation).toHaveBeenCalled();
+    expect(event.preventDefault).toHaveBeenCalled();
+  });
+
+  it('decks.getAll should called if a deck is removed', function () {
+    spyOn(decks, 'getAll');
+    var deck = decks.addNew();
+    scope.removeDeck(event, deck.options.id);
+    $rootScope.$digest();
+    expect(decks.getAll).toHaveBeenCalled();
   });
 });

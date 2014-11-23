1b981f6e6994ce9569e023a45115dabc7a120df3
Jörg Weber <weber.joerg.h@gmail.com>
2014-11-23 21:32:52 +0100

    * add card modal controller specs

---
+++ test/spec/controllers/card-modal.js
@@ -0,0 +1,35 @@
+'use strict';
+
+describe('Controller: DeckListController', function () {
+
+  // load the controller's module
+  beforeEach(module('mtgApp'));
+
+  var $rootScope,
+    scope,
+    modalInstance,
+    card = {};
+
+  beforeEach(inject(function ($injector) {
+    var $controller = $injector.get('$controller');
+
+    $rootScope = $injector.get('$rootScope');
+    scope = $rootScope.$new();
+    modalInstance = jasmine.createSpyObj('modalInstance', ['close', 'dismiss']);
+    $controller('CardModalController', {
+      $scope: scope,
+      $modalInstance: modalInstance,
+      card: card
+    });
+  }));
+
+  it('$scope.ok should call $modalInstance.close', function () {
+    scope.ok();
+    expect(modalInstance.close).toHaveBeenCalled();
+  });
+
+  it('$scope.cancel should call $modalInstance.dismiss', function () {
+    scope.cancel();
+    expect(modalInstance.dismiss).toHaveBeenCalled();
+  });
+});
